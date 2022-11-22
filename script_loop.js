652a0e606b27862812b3cceb0f9f0404394de496
Bruna Lekich Louro <brunalekich@gmail.com>
2022-11-22 14:21:54 -0300

    Add files via upload

---
+++ script_loop.js
@@ -0,0 +1,38 @@
+
+nameOfTheStudentArray = [];
+    
+function submit()
+{
+    var displayStudentArray = [];
+
+    for (var j = 1; j <= 4; j++) 
+    {
+        
+
+    }
+
+    console.log(nameOfTheStudentArray);
+
+    
+
+    for (var k = 0; k < lenghtOfNameOfStudentsArray; k++) 
+    {
+        
+    }
+
+   
+
+   
+}
+
+function sorting()
+{
+   
+    for (var k = 0; k < nameOfTheStudentArray.length; k++) 
+    {
+           }
+
+    
+}
+
+
